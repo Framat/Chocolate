a37d7b05e48995e63326d6ee6a3c2883ab2f9559
Framat <kozuch.mariusz1@gmail.com>
2016-09-15 14:16:30 +0200

    linked menu some small animations

--- js/app.js
+++ js/app.js
@@ -1,4 +1,16 @@
 $(document).ready(function() {
-    
+  // SLOW SCROLL
+  $(".slow").click(function() {
+      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+          if (target.length) {
+              $('html,body').animate({
+                  scrollTop: target.offset().top
+              }, 1300);
+              return false;
+          }
+      }
+  });
 
 });
